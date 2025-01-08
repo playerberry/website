a695e65014ef77e07b1067a088bdb2ac7971dc6d
yankikucuk <kendinikertenkelebek@me.com>
2025-01-09 01:58:06 +0300

    update

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -12,7 +12,7 @@ import { RouterLink } from "vue-router";
           >
         </div>
         <div class="uk-navbar-right">
-          <ul class="uk-navbar-nav">
+          <ul class="uk-navbar-nav uk-visible@s">
             <li>
               <RouterLink to="/">Hakkımızda</RouterLink>
             </li>
@@ -23,8 +23,29 @@ import { RouterLink } from "vue-router";
               <RouterLink to="/">Blog</RouterLink>
             </li>
           </ul>
+          <a
+            href="#"
+            class="uk-navbar-toggle uk-hidden@s"
+            uk-navbar-toggle-icon
+            uk-toggle="target: #sidenav"
+          ></a>
         </div>
       </div>
     </div>
   </nav>
+  <div id="sidenav" uk-offcanvas="flip: true" class="uk-offcanvas">
+    <div class="uk-offcanvas-bar">
+      <ul class="uk-nav">
+        <li>
+          <RouterLink class="uk-text-large" to="/">Hakkımızda</RouterLink>
+        </li>
+        <li>
+          <RouterLink class="uk-text-large" to="/">Projeler</RouterLink>
+        </li>
+        <li>
+          <RouterLink class="uk-text-large" to="/">Projeler</RouterLink>
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
